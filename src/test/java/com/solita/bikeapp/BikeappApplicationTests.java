0837d17976305209a436ac5603cf8f394ace84bb
jskodova <skodovajarmilatt@gmail.com>
2023-01-30 11:37:36 +0200

    Initial commit

---
+++ src/test/java/com/solita/bikeapp/BikeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solita.bikeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
