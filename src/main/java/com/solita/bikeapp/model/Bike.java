08e779fee101073de640bc889a6111714f7f1129
jskodova <skodovajarmilatt@gmail.com>
2023-01-30 19:33:49 +0200

    added constructor and getters/setters

--- src/main/java/com/solita/bikeapp/model/Bike.java
+++ src/main/java/com/solita/bikeapp/model/Bike.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @Entity
 @Table(name = "bike")
 public class Bike {
+
     @Id
     @Column(name = "departureTime")
     private Date departureTime;
@@ -38,6 +39,82 @@ public class Bike {
     @Id
     @Column(name = "duration")
     private int duration;
+
+
+    public Date getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Date departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Date getReturnTime() {
+        return returnTime;
+    }
+
+    public void setReturnTime(Date returnTime) {
+        this.returnTime = returnTime;
+    }
+
+    public int getDepStationID() {
+        return depStationID;
+    }
+
+    public void setDepStationID(int depStationID) {
+        this.depStationID = depStationID;
+    }
+
+    public String getDepStationName() {
+        return depStationName;
+    }
+
+    public void setDepStationName(String depStationName) {
+        this.depStationName = depStationName;
+    }
+
+    public int getRetStationID() {
+        return retStationID;
+    }
+
+    public void setRetStationID(int retStationID) {
+        this.retStationID = retStationID;
+    }
+
+    public String getRetStationName() {
+        return retStationName;
+    }
+
+    public void setRetStationName(String retStationName) {
+        this.retStationName = retStationName;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Bike(Date departureTime, Date returnTime, int depStationID, String depStationName, int retStationID, String retStationName, int distance, int duration) {
+        this.departureTime = departureTime;
+        this.returnTime = returnTime;
+        this.depStationID = depStationID;
+        this.depStationName = depStationName;
+        this.retStationID = retStationID;
+        this.retStationName = retStationName;
+        this.distance = distance;
+        this.duration = duration;
+    }
 
     public Bike() {
     }
