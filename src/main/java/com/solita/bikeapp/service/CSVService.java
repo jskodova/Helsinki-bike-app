a9d0bca5b19600239b585487fe4681143abcaa1d
jskodova <skodovajarmilatt@gmail.com>
2023-01-30 19:53:29 +0200

    created csv service for saving bikes to repository

---
+++ src/main/java/com/solita/bikeapp/service/CSVService.java
@@ -0,0 +1,31 @@
+package com.solita.bikeapp.service;
+
+import com.solita.bikeapp.model.Bike;
+import com.solita.bikeapp.method.CSVReader;
+import com.solita.bikeapp.repository.BikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class CSVService {
+    @Autowired
+    BikeRepository repository;
+
+    public void save(MultipartFile file) {
+        try {
+            List<Bike> bikes = CSVReader.csvToBike(file.getInputStream());
+            repository.saveAll(bikes);
+        } catch (IOException e) {
+            throw new RuntimeException("fail to store csv data: " + e.getMessage());
+        }
+    }
+
+    public List<Bike> getAllBikes() {
+        return repository.findAll();
+    }
+}
+}
