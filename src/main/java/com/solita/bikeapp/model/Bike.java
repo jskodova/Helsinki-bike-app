b3af17e4be2e4fdb2e6d774665f83887f22a2dff
jskodova <skodovajarmilatt@gmail.com>
2023-01-31 16:52:52 +0200

    updated Bike class with toString and @Override

--- src/main/java/com/solita/bikeapp/model/Bike.java
+++ src/main/java/com/solita/bikeapp/model/Bike.java
@@ -4,39 +4,30 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import org.apache.logging.log4j.message.AsynchronouslyFormattable;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.util.Date;
 
 
 
 @Entity
-@Table(name = "bike")
+@Table(name = "bikes")
 public class Bike {
 
-    @Id
     @Column(name = "departureTime")
     private Date departureTime;
-    @Id
     @Column(name = "returnTime")
     private Date returnTime;
     @Id
     @Column(name = "depStationID")
     private int depStationID;
-    @Id
     @Column(name = "depStationName")
     private String depStationName;
-    @Id
     @Column(name = "retStationID")
     private int retStationID;
-    @Id
     @Column(name = "retStationName")
     private String retStationName;
-    @Id
     @Column(name = "distance")
     private int distance;
-    @Id
     @Column(name = "duration")
     private int duration;
 
@@ -121,4 +112,34 @@ public class Bike {
 
     public Bike(java.util.Date departureTime, java.util.Date returnTime, String depStationID, String depStationName, String retStationID, String retStationName, int distance, int duration) {
     }
+
+    @Override
+    public String toString() {
+        return "Bike{" +
+                "departureTime=" + departureTime +
+                ", returnTime=" + returnTime +
+                ", depStationID=" + depStationID +
+                ", depStationName='" + depStationName + '\'' +
+                ", retStationID=" + retStationID +
+                ", retStationName='" + retStationName + '\'' +
+                ", distance=" + distance +
+                ", duration=" + duration +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
 }
