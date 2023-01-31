ad33d6ec04415e132ce4e163d3e2f1a7e6e6f156
jskodova <skodovajarmilatt@gmail.com>
2023-01-31 16:51:58 +0200

    added repository annotation and id type to repository

--- src/main/java/com/solita/bikeapp/repository/BikeRepository.java
+++ src/main/java/com/solita/bikeapp/repository/BikeRepository.java
@@ -1,6 +1,9 @@
 package com.solita.bikeapp.repository;
 
+import com.solita.bikeapp.model.Bike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BikeRepository extends JpaRepository {
+@Repository
+public interface BikeRepository extends JpaRepository<Bike, Integer>{
 }
