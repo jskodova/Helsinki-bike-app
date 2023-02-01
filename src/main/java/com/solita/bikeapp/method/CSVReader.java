5b917907fc12e5e34cef37d25cf29c6879fff596
jskodova <skodovajarmilatt@gmail.com>
2023-02-01 11:01:23 +0200

    reworked the methods for csv checking and csv parsing

--- src/main/java/com/solita/bikeapp/method/CSVReader.java
+++ src/main/java/com/solita/bikeapp/method/CSVReader.java
@@ -4,31 +4,38 @@ import com.solita.bikeapp.model.Bike;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CSVReader {
     public static String TYPE = "text/csv";
-    static String[] headers = { "departureTime", "returnTime", "depStationID", "depStationName", "retStationID", "retStationName", "distance", "duration"};
+    static String[] headers = {"departureTime", "returnTime", "depStationID", "depStationName", "retStationID", "retStationName", "distance", "duration"};
 
-    public static boolean hasCSVFormat(MultipartFile file) {
-
-        if (!TYPE.equals(file.getContentType())) {
-            return false;
+    public static boolean isCSV(File csvfile) {
+        try {
+            FileReader reader = new FileReader(csvfile);
+            Scanner scanner = new Scanner(reader);
+            String line = scanner.nextLine();
+            String[] values = line.split(",");
+            if (values.length > 1) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        return true;
+        return false;
     }
-    public static List<Bike> csvToBike(InputStream is) {
-        try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-             CSVParser csvParser = new CSVParser(fileReader,
+
+    public static List<Bike> csvToBikes(File file) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file));
+             CSVParser csvParser = new CSVParser(br,
                      CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())) {
 
             List<Bike> bikes = new ArrayList<>();
@@ -46,7 +53,6 @@ public class CSVReader {
                         Integer.parseInt(csvRecord.get("distance")),
                         Integer.parseInt(csvRecord.get("duration"))
                 );
-
                 bikes.add(bike);
             }
 
@@ -55,5 +61,4 @@ public class CSVReader {
             throw new RuntimeException("fail to parse CSV file: " + e.getMessage());
         }
     }
-
 }
