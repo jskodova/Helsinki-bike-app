96eaa3295f01ad4f2725c5f6459e3636f2f66568
jskodova <skodovajarmilatt@gmail.com>
2023-02-03 00:26:39 +0200

    small change to repository,removed unnecessary import

--- src/main/java/com/solita/bikeapp/repository/BikeRepository.java
+++ src/main/java/com/solita/bikeapp/repository/BikeRepository.java
@@ -2,8 +2,7 @@ package com.solita.bikeapp.repository;
 
 import com.solita.bikeapp.model.Bike;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BikeRepository extends JpaRepository<Bike, Integer>{
+public interface BikeRepository extends JpaRepository<Bike, Long> {
+
 }
