adee5f91be67ebddd9ef4b026beed43128154fd6
jskodova <skodovajarmilatt@gmail.com>
2023-02-03 00:25:40 +0200

    reworked service class to work with updated csvreader

--- src/main/java/com/solita/bikeapp/service/CSVService.java
+++ src/main/java/com/solita/bikeapp/service/CSVService.java
@@ -6,27 +6,24 @@ import com.solita.bikeapp.repository.BikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
 
 @Service
 public class CSVService {
     @Autowired
     BikeRepository repository;
+    @Autowired
+    CSVReader reader;
 
-    public void save(File file) {
-        List<Bike> bikes = CSVReader.csvToBikes(file);
-        repository.saveAll(bikes);
+    public void saveAll() throws IOException {
+        reader.readCSV();
+        repository.saveAll(CSVReader.bikes);
     }
 
     public List<Bike> getAllBikes() {
         return repository.findAll();
     }
-
-    public void saveAllFilesInRepository() {
-        File[] files = new File("resources/csv").listFiles();
-        for (File file : files) {
-            save(file);
-        }
-    }
 }
+
