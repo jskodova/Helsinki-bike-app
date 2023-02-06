831b28ac3fb4e8cf55cc93924c024ad7df98717a
jskodova <skodovajarmilatt@gmail.com>
2023-02-06 21:52:48 +0200

    added a function for reading through addresses and counting their occurence in the journeys

--- src/main/java/com/solita/bikeapp/method/CSVReader.java
+++ src/main/java/com/solita/bikeapp/method/CSVReader.java
@@ -1,7 +1,9 @@
 package com.solita.bikeapp.method;
 
 import com.solita.bikeapp.entity.JourneyEntity;
+import com.solita.bikeapp.entity.StationEntity;
 import com.solita.bikeapp.repository.JourneyRepository;
+import com.solita.bikeapp.repository.StationRepository;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -23,19 +25,29 @@ public class CSVReader {
 
     //List of csv files
     List<String> fileNames;
+    List<String> addresses;
 
     //setting headers for the csv files so they can be ignored, also avoids the byte reading error with CSV Parser
     static String[] HEADERS = {"Departure", "Return", "Departure station id", "Departure station name", "Return station id", "Return station name", "Covered distance (m)", "Duration (sec.)"};
+    static String[] HEADERS2 = {"FID", "ID", "Nimi", "Namn", "Name", "Osoite", "Adress", "Kaupunki", "Stad", "Operaattor", "Kaapasiteet", "x", "y"};
 
     public static List<JourneyEntity> journeys = new ArrayList<>();
+    public static List<StationEntity> stations = new ArrayList<>();
     @Autowired
     public final JourneyRepository journeyRepository;
 
+    @Autowired
+    public final StationRepository stationRepository;
+
+
     //this constructor with added csv files, only one is added because of size restrictions
-    public CSVReader(List<String> fileNames, JourneyRepository journeyRepository) {
+    public CSVReader(List<String> fileNames, List<String> addresses, JourneyRepository journeyRepository, StationRepository stationRepository) {
+        this.addresses = addresses;
         this.fileNames = fileNames;
         this.journeyRepository = journeyRepository;
+        this.stationRepository = stationRepository;
         fileNames.add("src/main/resources/csv/2021-05.csv");
+        addresses.add("src/main/resources/csv/addresses.csv");
         //these csv were too big to put on github
         //fileNames.add("src/main/resources/csv/2021-06.csv");
         //fileNames.add("src/main/resources/csv/2021-07.csv");
@@ -69,18 +81,62 @@ public class CSVReader {
                     journey.setDepStationName(csvRecord.get("Departure station name"));
                     journey.setRetStationID(Integer.parseInt(csvRecord.get("Return station id")));
                     journey.setRetStationName(csvRecord.get("Return station name"));
-                    journey.setDistance((Float.parseFloat(csvRecord.get("Covered distance (m)"))) / 10);
+                    journey.setDistance((Float.parseFloat(csvRecord.get("Covered distance (m)"))) / 1000);
                     journey.setDuration(Integer.parseInt(csvRecord.get("Duration (sec.)")));
 
                     if (journey.getDistance() > 0.01 && journey.getDuration() > 10) {
                         journeys.add(journey);
                         journeyRepository.save(journey);
                     }
-                    journeys.add(journey);
-                    journeyRepository.save(journey);
                 }
             } finally {
                 fileReader.close();
+                csvParser.close();
+            }
+        }
+    }
+
+    public void readAddress() throws IOException {
+        for (String address : addresses) {
+            final BufferedReader addressReader = new BufferedReader((new InputStreamReader(new FileInputStream(address), StandardCharsets.UTF_8)));
+            System.out.print("Trying to read csv");
+
+            CSVParser addressParser = new CSVParser(addressReader,
+                    CSVFormat.Builder.create()
+                            .setHeader(HEADERS2)
+                            .setIgnoreEmptyLines(true)
+                            .setSkipHeaderRecord(true)
+                            .setTrim(true)
+                            .build());
+            try {
+                for (final CSVRecord addressRecord : addressParser) {
+                    StationEntity station = new StationEntity();
+                    station.setStationName(addressRecord.get("Nimi"));
+                    station.setStationID(Integer.parseInt(addressRecord.get("ID")));
+                    station.setAddress(addressRecord.get("Osoite"));
+                    station.setCity(addressRecord.get("Kaupunki"));
+                    station.setCoorX(Float.parseFloat(addressRecord.get(("x"))));
+                    station.setCoorY(Float.parseFloat(addressRecord.get(("y"))));
+
+                    int occurrenceDep = 0;
+                    int occurrenceRet = 0;
+                    List<JourneyEntity> journeys = journeyRepository.findAll();
+                    for (JourneyEntity journey : journeys) {
+                        if (station.getStationName().equals(journey.getDepStationName())) {
+                            occurrenceDep++;
+                        }
+                        if (station.getStationName().equals(journey.getRetStationName())) {
+                            occurrenceRet++;
+                        }
+                    }
+                    station.setCountDep(occurrenceDep);
+                    station.setCountRet(occurrenceRet);
+                    stations.add(station);
+                    stationRepository.save(station);
+                }
+            } finally {
+                addressReader.close();
+                addressParser.close();
             }
         }
     }
--- src/main/java/com/solita/bikeapp/service/AppService.java
+++ src/main/java/com/solita/bikeapp/service/AppService.java
@@ -1,8 +1,10 @@
 package com.solita.bikeapp.service;
 
 import com.solita.bikeapp.entity.JourneyEntity;
+import com.solita.bikeapp.entity.StationEntity;
 import com.solita.bikeapp.method.CSVReader;
 import com.solita.bikeapp.repository.JourneyRepository;
+import com.solita.bikeapp.repository.StationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,8 @@ public class AppService {
     JourneyRepository journeyRepository;
     @Autowired
     CSVReader reader;
+    @Autowired
+    private StationRepository stationRepository;
 
     //saves all entities in journeyrepository to the db
     public void saveAll() throws IOException {
@@ -22,10 +26,18 @@ public class AppService {
         journeyRepository.saveAll(CSVReader.journeys);
     }
 
+    public void saveAllStations() throws IOException {
+        reader.readAddress();
+        stationRepository.saveAll(CSVReader.stations);
+    }
+
     //gets all journeys from the db
     public List<JourneyEntity> getAllJourneys() {
         return journeyRepository.findAll();
     }
 
+    public List<StationEntity> getAllStations() {
+        return stationRepository.findAll();
+    }
 }
 
